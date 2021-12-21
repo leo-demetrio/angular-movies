d721d3e5af3eb3570f3c4047d14aba17bce69f4e
leo-demetrio <leocdemetrio@gmail.com>
2021-12-21 18:54:54 -0300

    feat: form validations and film registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import {  HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -26,7 +27,8 @@ import { FilmesModule } from './filmes/filmes.module';
     LayoutModule,
     MaterialModule,
     AppRoutingModule,
-    FilmesModule
+    FilmesModule,
+    HttpClientModule
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
+import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -9,16 +12,50 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
+  generos: Array<string>;
+  
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    public validacao: ValidarCamposService,
+    private fb: FormBuilder,
+    private filmeService: FilmesService
+    ) { }
 
-  ngOnInit() {
+  get f() {
+    return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
 
     this.cadastro = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dataLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDB: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
     });
 
+    this.generos = ['Ação', "Romance", 'Aventura', 'Terror', 'Ficção ciêntifica', 'Comédia', 'Drama'];
+  }
+  submit(): void{
+    this.cadastro.markAllAsTouched();
+    if(this.cadastro.invalid) return;
+    const filme = this.cadastro.getRawValue() as Filme;
+    this.salvar(filme);
+  }
+  reiniciarForm(): void{
+    this.cadastro.reset();
+  }
+  private salvar(filme: Filme): void {
+    this.filmeService.salvar(filme).subscribe(() => {
+      alert('sucesso');
+      this.reiniciarForm();
+    }),
+    () => {
+      alert("erro");
+    };
   }
 
 }
